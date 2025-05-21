35a59f135f8f2af0920612ff428483f91efea754
tokashawky <tokashawky@localhost.localdomain>
2025-05-21 04:53:11 +0300

    feat: integrate EC2 with TLS-generated SSH key pair and store keys in Secrets Manager

--- EC2s.tf
+++ EC2s.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "Bastion" {
   subnet_id =  aws_subnet.subnets["PublicSubnet1"].id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  key_name = aws_key_pair.key.key_name
+  key_name = aws_key_pair.public_key_pair.key_name
   
   tags = {
     Name = "Bastion"
@@ -21,7 +21,7 @@ resource "aws_instance" "application" {
   instance_type = var.instance_type
   subnet_id = aws_subnet.subnets["PublicSubnet1"].id
   vpc_security_group_ids = [aws_security_group.allow_ssh_3000.id]
-  key_name = aws_key_pair.key.key_name
+  key_name = aws_key_pair.public_key_pair.key_name    
   
   tags = {
     Name = "application"
--- key.tf
+++ key.tf
@@ -1,4 +1,29 @@
-resource "aws_key_pair" "key" {
+# you will need to make : terraform init -upgrade
+resource "tls_private_key" "secret_key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "public_key_pair" {
   key_name   = "sshkey"
-  public_key = file("/home/tokashawky/.ssh/id_rsa.pub")
+  public_key = tls_private_key.secret_key_pair.public_key_openssh
+}
+
+resource "aws_secretsmanager_secret" "private_key" {
+  name = "private_key"
+  description = "ssh private_key"
+  recovery_window_in_days = 0
+}
+resource "aws_secretsmanager_secret" "public_key" {
+  name = "public_key"
+  description = "ssh public_key"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "secret_key_value" {
+  secret_id     = aws_secretsmanager_secret.private_key.id
+  secret_string = tls_private_key.secret_key_pair.private_key_pem
+}
+resource "aws_secretsmanager_secret_version" "public_key_value" {
+  secret_id     = aws_secretsmanager_secret.public_key.id
+secret_string = tls_private_key.secret_key_pair.public_key_openssh
 }
